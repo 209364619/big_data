b86e950f6e6be268dd96b434632c7c1409ecc7e7
hph <209364619@qq.com>
2018-10-25 10:03:18 +0800

    first commit

---
+++ src/main/java/KafkaStorm/MykafkaSpout.java
@@ -0,0 +1,63 @@
+package storm;
+
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
+import backtype.storm.generated.AlreadyAliveException;
+import backtype.storm.generated.InvalidTopologyException;
+import backtype.storm.spout.SchemeAsMultiScheme;
+import backtype.storm.topology.TopologyBuilder;
+import storm.kafka.KafkaSpout;
+import storm.kafka.SpoutConfig;
+import storm.kafka.StringScheme;
+import storm.kafka.ZkHosts;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MykafkaSpout {
+
+    /**
+     * @param args
+     * @throws
+     */
+    public static void main(String[] args)  {
+        // TODO Auto-generated method stub
+
+        String topic = "data" ;
+        ZkHosts zkHosts = new ZkHosts("192.168.1.231:2181,192.168.1.232:2181,192.168.1.233:2181");
+        SpoutConfig spoutConfig = new SpoutConfig(zkHosts, topic,
+                "",
+                "MyTrack") ;
+        List<String> zkServers = new ArrayList<String>() ;
+        zkServers.add("192.168.1.231");
+        zkServers.add("192.168.1.232");
+        zkServers.add("192.168.1.233");
+        spoutConfig.zkServers = zkServers;
+        spoutConfig.zkPort = 2181;
+        spoutConfig.socketTimeoutMs = 60 * 1000 ;
+        spoutConfig.scheme = new SchemeAsMultiScheme(new StringScheme()) ;
+
+        TopologyBuilder builder = new TopologyBuilder() ;
+        builder.setSpout("spout", new KafkaSpout(spoutConfig) ,1) ;
+        builder.setBolt("bolt1", new MyKafkaBolt(), 1).shuffleGrouping("spout") ;
+
+        Config conf = new Config ();
+        conf.setDebug(false) ;
+
+        if (args.length > 0) {
+            try {
+                StormSubmitter.submitTopology(args[0], conf, builder.createTopology());
+            } catch (AlreadyAliveException e) {
+                e.printStackTrace();
+            } catch (InvalidTopologyException e) {
+                e.printStackTrace();
+            }
+        }else {
+            LocalCluster localCluster = new LocalCluster();
+            localCluster.submitTopology("mytopology", conf, builder.createTopology());
+        }
+
+    }
+
+}
